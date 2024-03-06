eb0cc2b62cdd59400705e004de21894a7bb65c8c
Orcani <140945075+Orcani@users.noreply.github.com>
2024-03-06 13:44:38 +0100

    New commit

---
+++ mdr/mdr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mdr
+//
+//  Created by Apprenant 38 on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Changement")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mdr/mdr/mdrApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdrApp.swift
+//  mdr
+//
+//  Created by Apprenant 38 on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct mdrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
